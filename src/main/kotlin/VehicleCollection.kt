b2075c87c1e65b0fad4c5e6772ff12c3816d97ca
Stepan Karasov <stepan.karasov@gmail.com>
2022-04-04 15:36:59 +1000

    ДЗ №2

---
+++ src/main/kotlin/VehicleCollection.kt
@@ -0,0 +1,47 @@
+package ru.otus.okb.first
+
+sealed class VehicleCollection(
+    val name: String,
+    val vehiclesCount: Int,
+    val vehiclesList: List<Vehicle>
+) {
+    class Car(name: String, vehiclesCount: Int, vehiclesList: List<Vehicle> = emptyList()) :
+        VehicleCollection(name, vehiclesCount, vehiclesList)
+
+    class Truck(name: String, vehiclesCount: Int, vehiclesList: List<Vehicle> = emptyList()) :
+        VehicleCollection(name, vehiclesCount, vehiclesList)
+}
+
+abstract class VehicleCollectionBuilder {
+    var name: String = ""
+    var vehiclesCount: Int = 0
+    var vehiclesList: MutableList<Vehicle> = mutableListOf()
+
+    fun add(vehicle: Vehicle) {
+        vehiclesList.add(vehicle)
+    }
+
+    fun add(vehicleBuilder: VehicleBuilder) {
+        add(vehicleBuilder.build())
+    }
+
+    abstract fun build(): VehicleCollection
+}
+
+class CarBuilder : VehicleCollectionBuilder() {
+    override fun build(): VehicleCollection.Car = VehicleCollection.Car(name, vehiclesCount, vehiclesList.toList())
+
+    fun vehicle(function: VehicleBuilder.() -> Unit) = add(VehicleBuilder().apply(function))
+}
+
+class TruckBuilder : VehicleCollectionBuilder() {
+    override fun build(): VehicleCollection.Truck = VehicleCollection.Truck(name, vehiclesCount, vehiclesList.toList())
+
+    fun vehicle(function: VehicleBuilder.() -> Unit) = add(VehicleBuilder().apply(function))
+}
+
+fun car(function: CarBuilder.() -> Unit): VehicleCollection.Car = CarBuilder().apply(function).build()
+
+fun truck(function: TruckBuilder.() -> Unit): VehicleCollection.Truck = TruckBuilder().apply(function).build()
+inline fun <reified T : VehicleCollection> vehiclesCollectionName(vehiclesCollection: T): String =
+    "Collection name is ${vehiclesCollection.name}"
---
+++ src/test/kotlin/DslTest.kt
@@ -0,0 +1,69 @@
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import ru.otus.okb.first.*
+import kotlin.test.assertEquals
+
+class DslTest {
+    @Test
+    fun vehicleCollectionTest() {
+        var carsCollection: VehicleCollection = car {
+            name = "My cars"
+            vehiclesCount = 8
+
+            vehicle {
+                brand = "Toyota"
+                model = "Hiace"
+                price = 600000
+            }
+            vehicle {
+                brand = "Suzuki"
+                model = "Swift"
+                price = 700000
+            }
+            vehicle {
+                brand = "Mazda"
+                model = "Demio"
+                price = 500000
+            }
+        }
+
+        val carsCollectionName: String = vehiclesCollectionName(carsCollection)
+
+        assertEquals("My cars", carsCollection.name)
+        assertEquals(8, carsCollection.vehiclesCount)
+        assertEquals(3, carsCollection.vehiclesList.size)
+        assertTrue(
+            "Incorrect vehicle brand!",
+            carsCollection.vehiclesList.first().brand.equals("Toyota")
+        )
+        assertTrue(
+            "Incorrect vehicle model!",
+            carsCollection.vehiclesList.first().model.equals("Hiace")
+        )
+        assertTrue(
+            "Incorrect vehicles price!",
+            carsCollection.vehiclesList.first().price == 600000
+        )
+        assertEquals("Collection name is My cars", carsCollectionName)
+    }
+
+    @Test
+    fun vehicleMileageTest() {
+        var vehicle: Vehicle = vehicle {
+            brand = "Suzuki"
+            model = "Swift"
+            price = 700000
+            mileage = 0
+            nextTO = 5000
+        }
+        vehicle mileageUp 2000
+        vehicle + 3000
+
+        val targetNextTO = doTO(vehicle, 10000)
+
+        assertEquals("Suzuki", vehicle.brand)
+        assertEquals("Swift", vehicle.model)
+        assertEquals(5000, vehicle.mileage)
+        assertTrue(targetNextTO in 14000..16000)
+    }
+}
